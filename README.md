31136683d710fbfead8d301b1b5351c5cd183b79
Darnell <darnellatsimon@gmail.com>
2019-03-05 02:08:18 -0800

    Structured back end configured login and sign up & seeded the database

--- README.md
+++
@@ -1,3 +0,0 @@
-#Wayfarer-BackEnd
-
-MongoDB Backend for CRUD We have been commissioned to build a travel community, code-named Project Wayfarer for now, for users to share tips (AKA posts) about their favorite locations around the world.
--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-
-// we are change to 3001 since react will run on port 3000
-app.listen(process.env.PORT || 3001,  () => console.log('Server is up and running!!! Example app listening at http://localhost:3001/'));
